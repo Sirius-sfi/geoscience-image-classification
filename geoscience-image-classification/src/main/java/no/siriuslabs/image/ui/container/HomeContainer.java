5ee4a942dea0f3a4c557a5f2af458c18fbddaafa
ostahl <ostahl@ifi.uio.no>
2019-03-14 14:34:39 +0100

    Added basic application title to home screen

--- geoscience-image-classification/src/main/java/no/siriuslabs/image/ui/container/HomeContainer.java
+++ geoscience-image-classification/src/main/java/no/siriuslabs/image/ui/container/HomeContainer.java
@@ -1,14 +1,20 @@
 package no.siriuslabs.image.ui.container;
 
+import eu.webtoolkit.jwt.AlignmentFlag;
 import eu.webtoolkit.jwt.WContainerWidget;
+import eu.webtoolkit.jwt.WHBoxLayout;
 import eu.webtoolkit.jwt.WText;
 import no.siriuslabs.image.AbstractAnnotationApplication;
+
+import java.util.EnumSet;
 
 public class HomeContainer extends AbstractAnnotationContainer {
 
 	public HomeContainer(AbstractAnnotationApplication application, WContainerWidget parent) {
 		super(application, parent);
 
-		addWidget(new WText("Home"));
+		WHBoxLayout layout = new WHBoxLayout();
+		layout.addWidget(new WText("<h1>SIRIUS GeoAnnotator</h1>"), 1, EnumSet.of(AlignmentFlag.AlignCenter));
+		setLayout(layout);
 	}
 }
