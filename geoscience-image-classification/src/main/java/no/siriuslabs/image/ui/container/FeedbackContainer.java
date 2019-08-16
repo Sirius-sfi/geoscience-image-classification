18acae64ece7c44d3723a4ce9a908dcbdc0d2b02
ostahl <ostahl@ifi.uio.no>
2019-08-16 10:24:16 +0200

    Added final text to feedback page

--- geoscience-image-classification/src/main/java/no/siriuslabs/image/ui/container/FeedbackContainer.java
+++ geoscience-image-classification/src/main/java/no/siriuslabs/image/ui/container/FeedbackContainer.java
@@ -47,21 +47,24 @@ public class FeedbackContainer extends AbstractAnnotationContainer {
 		WVBoxLayout layout = new WVBoxLayout();
 		addSpacerWidgetStretched(layout);
 
-		WText textIntro = new WText("Introduction text here     Introduction text here     Introduction text here");
+		WText textIntro = new WText("<b>We appreciate your feedback regarding this tool and ontology. Please take a look at the most common cases below and drop us an email by clicking the button below. " +
+				"Please note that we might not be able to reply to every email, but we will take all suggestions into account.</b>");
 		layout.addWidget(textIntro, 0, EnumSet.of(AlignmentFlag.AlignLeft, AlignmentFlag.AlignMiddle));
 
 		addSpacerWidget(layout);
 
 		WText textBugs = new WText("<b>Bug reports:</b>" +
-				"<p><i>Descriptive text here     Descriptive text here     Descriptive text here     Descriptive text here     Descriptive text here</i></p>");
+				"<p>If you encounter an error in the software like something just not doing what it should do or you being “thrown back” to the startup page, please let us know. " +
+				"Send us a description of what you did, what went wrong and if it was reproducible. The more detailed, the better – it is hard to fix errors for situations you cannot understand or recreate. " +
+				"If possible you can attach a screenshot, too.</p>");
 		layout.addWidget(textBugs, 0, EnumSet.of(AlignmentFlag.AlignLeft, AlignmentFlag.AlignMiddle));
 
 		WText textFeatures = new WText("<b>Feature suggestions:</b>" +
-				"<p><i>Descriptive text here     Descriptive text here     Descriptive text here     Descriptive text here     Descriptive text here</i></p>");
+				"<p>If you think some feature or functionality is missing in the tool, please send us a description of what you would like to see and how this could improve the tool and make your daily work easier.</p>");
 		layout.addWidget(textFeatures, 0, EnumSet.of(AlignmentFlag.AlignLeft, AlignmentFlag.AlignMiddle));
 
 		WText textOntology = new WText("<b>Ontology requests:</b>" +
-				"<p><i>Descriptive text here     Descriptive text here     Descriptive text here     Descriptive text here     Descriptive text here</i></p>");
+				"<p>If you find something missing in the vocabulary of the ontology (subjects, predicates, objects or relations) or think something should be renamed in a different way, please explain what you would like to have changed and why.</p>");
 		layout.addWidget(textOntology, 0, EnumSet.of(AlignmentFlag.AlignLeft, AlignmentFlag.AlignMiddle));
 
 		WHBoxLayout buttonLayout = initializeButtonLine();
